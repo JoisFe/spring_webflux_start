6ce194e6ddbfe2656b3a6526a7fb22f75a7a315f
JoisFe <rhsnqk@gmail.com>
2023-09-07 22:53:59 +0900

    Hello Reactor 출력을 통한 Reactor 알아보기
    - Flux를 이용한 데이터 생성하여 제공, 데이터 가공, 처리 3단계 알아보기

---
+++ helloreactor/src/main/java/com/example/HelloreactorApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class HelloreactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloreactorApplication.class, args);
+
+        Flux<String> sequence = Flux.just("Hello", "Reactor");
+        sequence.map(String::toLowerCase)
+            .subscribe(System.out::println);
+    }
+}
